98003ca756d03a4f7359e8355244ca7d715ef2a7
Mariafernandarj <mafer_rj@ciencias.unam.mx>
2026-09-10 14:21:18 -0600

    Busqueda Binaria implementada

--- tsp-rs/internal/rs/enfriamiento.go
+++ tsp-rs/internal/rs/enfriamiento.go
@@ -1,10 +1,10 @@
 package rs
 
 import (
-// "fmt"
-// "math"
-// "math/rand"
-// "tsp-rs/internal/model"
+	// "fmt"
+	"math"
+	// "math/rand"
+	// "tsp-rs/internal/model"
 )
 
 func AceptacionPorUmbrales(rng *rand.Rand, grafica *GraficaTSP, T float64, s []int, N float64, params Parametros) ([]int, float64, error) {
@@ -39,4 +39,27 @@ func AceptacionPorUmbrales(rng *rand.Rand, grafica *GraficaTSP, T float64, s []i
 		T *= params.Phi
 	}
 	return mejorS, mejorCosto, nil
+}
+
+func BusquedaBinaria(rng *rand.Rand, grafica *GraficaTSP, s []int, T1, T2, Aceptacion, epsilonP, N float64, iteraciones int) (float64, error) {
+	Tm := (T1 + T2) / 2
+	if T2-T1 < epsilonP {
+		return Tm, nil
+	}
+
+	p, err := PorcentajeAceptados(rng, grafica, s, Tm, N, iteraciones)
+
+	if err != nil {
+		return 0, err
+	}
+
+	if math.Abs(Aceptacion-p) < epsilonP {
+		return Tm, nil
+	}
+
+	if p > Aceptacion {
+		return BusquedaBInaria(rng, grafica, s, T1, Tm, Aceptacion, epsilonP, N, iteraciones)
+	}
+
+	return BusquedaBinaria(rng, grafica, s, Tm, T2, Aceptacion, epsilonP, N, iteraciones)
 }
